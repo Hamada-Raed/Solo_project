64a34b69a45ce099a48ae4964a084e440e03a782
Hamada-Raed <stu.no1663@gmail.com>
2023-07-08 20:40:22 +0300

    commit

--- Solo/soloapp/urls.py
+++ Solo/soloapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('QandA', views.QandA),
+    path('all_subject', views.all_subject)
 ]
--- Solo/soloapp/views.py
+++ Solo/soloapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request): 
-    return render (request, 'index.html')
+    return render(request, 'index.html')
+
+def QandA(request): 
+    return render(request, 'QandA.html' )
+
+def all_subject(request): 
+    return render(request, 'courses.html')
