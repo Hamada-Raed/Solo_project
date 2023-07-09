fe51d3e1e815148be0d14f57dfb21f10a248ec34
Hamada-Raed <stu.no1663@gmail.com>
2023-07-09 08:07:41 +0300

    commit

--- Solo/soloapp/urls.py
+++ Solo/soloapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('aboutus', views.aboutus),
     path('QandA', views.QandA),
-    path('all_subject', views.all_subject)
+    path('all_subject', views.all_subject),
+    path('signin', views.signin),
 ]
--- Solo/soloapp/views.py
+++ Solo/soloapp/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def index(request): 
     return render(request, 'index.html')
 
+def aboutus(request):
+    return render(request, 'aboutus.html')
+    
 def QandA(request): 
     return render(request, 'QandA.html' )
 
 def all_subject(request): 
     return render(request, 'courses.html')
+
+def signin(request): 
+    return render(request, 'logregister.html')
